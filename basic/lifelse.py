619af7418c727c3f32c1a975ba9cbede3c95d7e0
haleypetersen <haleykupetersen@gmail.com>
2021-03-12 22:41:32 +0000

    add more files

--- basic/lifelse.py
+++ basic/lifelse.py
@@ -1,16 +1,26 @@
+i = 1
+total = 0
+while i <= 10:
+    total = total + i
+    if i % 2 == 0:
+        print("i = {0} Total = {1} ".format(i, total))
+    i = i + 1
 
-who = input("Who are you?")
+print("Final total: ", total)
 
-if who == "dog!":
-    print("bark!")
-elif who == "cat!":
-  print("Meow!")
-else:
-    print("Nothing")
 
-age = int(input("How old are you?"))
+#who = input("Who are you?")
+#
+#if who == "dog!":
+#    print("bark!")
+#elif who == "cat!":
+#  print("Meow!")
+#else:
+#    print("Nothing")
+#
+#age = int(input("How old are you?"))
 # str to convert int to string.
-if age < 16:
-    print("need to be 16 years old!")
-else:
-    print("you are ok to go!")
+#if age < 16:
+#    print("need to be 16 years old!")
+#else:
+#    print("you are ok to go!")
